c4182da1100f275ebe4d04c93214575618b57e9c
sierraskyewallace <sskwallace@gmail.com>
2021-12-30 12:43:24 -0800

    added recipe model

--- db/migrate/20211230203810_create_recipes.rb
+++ db/migrate/20211230203810_create_recipes.rb
@@ -1,7 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :description
+      t.string :image_url
+      t.integer :recipe_category_id
+      t.string :instructions
+      t.string :ingredients
       t.timestamps
     end
   end
