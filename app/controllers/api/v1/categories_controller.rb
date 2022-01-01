bd91e33685d51a014a8d0e9cfabbee23cb2fd97b
sierraskyewallace <sskwallace@gmail.com>
2022-01-01 12:46:11 -0800

    added actions to categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,4 +5,40 @@ class Api::V1::CategoriesController < ApplicationController
         render json: CategorySerializer.new(categories)
     end
 
+    def show
+        category = Category.find(params[:id])
+        render json: CategorySerializer.new(category)
+    end
+
+    def new
+        category = Category.new(category_params)
+    end
+
+    def create
+        category = Category.create(category_params)
+            if category.save!
+            render json: CategorySerializer.new(category)
+            else
+            render json: {error: 'Error creating category'}
+            end
+        end
+
+    def update
+        category = Category.find(params[:id])
+        category.update(category_params)
+        render json: CategorySerializer.new(category)
+    end
+    
+    def destroy
+        category = Category.find(params[:id])
+        category.destroy
+        render json: CategorySerializer.new(category)
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :description)
+    end
+
 end
