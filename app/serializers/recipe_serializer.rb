5ac9b731be933e8a4806247a73a697d49c010b57
sierraskyewallace <sskwallace@gmail.com>
2022-01-01 12:11:57 -0800

    index for both controllers done

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :instructions, :ingredients, :category_id, :image_url
+  attributes :name, :description, :instructions, :ingredients, :category_id, :image_url, :category
 end
