bb896efd25fd8dff2c163da06b41dbc996dbfd5c
sierraskyewallace <sskwallace@gmail.com>
2022-01-01 12:45:08 -0800

    Added create show and new actions and params method to recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,4 +5,42 @@ class Api::V1::RecipesController < ApplicationController
         render json: RecipeSerializer.new(recipes)
     end
 
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: RecipeSerializer.new(recipe)
+    end
+
+    def new 
+        recipe = Recipe.new(recipe_params)
+    end
+
+    def create
+        recipe = Recipe.create(recipe_params)
+            if recipe.save!
+            render json: RecipeSerializer.new(recipe)
+            else
+            render json: {error: 'Error creating recipe'}
+            end
+        end
+end
+
+
+    def update
+        recipe = Recipe.find(params[:id])
+        recipe.update(recipe_params)
+        render json: RecipeSerializer.new(recipe)
+    end
+
+    def destroy
+        recipe = Recipe.find(params[:id])
+        recipe.destroy
+        render json: RecipeSerializer.new(recipe)
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :image_url, :category_id, :instructions, :ingredients)
+    end
+
 end
