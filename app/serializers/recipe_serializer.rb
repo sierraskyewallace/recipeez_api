360b2884a32b0435791c10bb95ab615079904e8c
sierraskyewallace <sskwallace@gmail.com>
2022-01-14 15:24:08 -0800

    done

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,8 +1,9 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :instructions, :image_url, :ingredients, :tag_ids
+  attributes :id, :name, :description, :instructions, :image_url, :ingredients, :tags
 
- 
+ has_many :recipe_tags
+  has_many :tags, through: :recipe_tags
 
 
 end
