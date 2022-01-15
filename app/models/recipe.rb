dff8db239587f3201fe746f626c243ba2629e75a
sierraskyewallace <sskwallace@gmail.com>
2022-01-15 09:29:53 -0800

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,11 @@
 class Recipe < ApplicationRecord
 
 
+validates :name, presence: true
+validates :description, presence: true
+
+validates :instructions, presence: true
+validates :ingredients, presence: true
 
 
 end
